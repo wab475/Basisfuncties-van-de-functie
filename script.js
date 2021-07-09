0360bdb25e7353aa277c09df53f194aadc31b384
wab475 <84964630+wab475@users.noreply.github.com>
2021-07-09 21:18:13 +0200

    Opdracht_017 Basisfuncties van de functie

---
+++ script.js
@@ -0,0 +1,21 @@
+const makePizza = function () {
+  console.log("Rol het deeg uit");
+  console.log("Smeer de tomatensaus over het deeg");
+  console.log("Strooi de kaas over de pizza");
+  console.log("Verdeel de groente en het vlees over de pizza");
+  console.log("Doe de pizza in de oven");
+};
+
+const makeSushi = function () {
+  console.log("Kook de rijst");
+  console.log("Bedek de sushimat met de rijst");
+  console.log("Voeg de krab, avocado en komkommer toe");
+  console.log("Rol het geheel strak p[");
+};
+
+makePizza();
+makeSushi();
+makeSushi();
+makePizza();
+makeSushi();
+
